70deceddbf61577f2a6e1fceccfd745a08f44d05
Jnasci50 <jairoarea1@hotmail.com>
2021-11-06 02:23:57 -0300

    Atuaização

---
+++ Programas Java/Curso Udemy/Array_Collection/Array Foreach/src/DesafioArray.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class DesafioArray {
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Quantas notas voçê gostaria de inserir");
+
+        Scanner indece = new Scanner(System.in);
+
+        int index = indece.nextInt();
+
+        Double notas [] = new Double[index];
+
+        int addNotas = 1;
+        double totalnotas =0 ;
+
+
+
+        for (Double PercorreNota:notas) {
+
+                System.out.println("digite sua: " + addNotas + " nota");
+
+                Scanner InNota = new Scanner(System.in);
+
+                Double InNotaData = InNota.nextDouble();
+
+                System.out.println("Sua" + addNotas +" nota digitada foi: " + InNotaData);
+
+                addNotas ++ ;
+
+                totalnotas += InNotaData;
+
+
+            }
+
+
+        double media  = totalnotas / notas.length;
+        System.out.println("Sua média é: " + media);
+
+        if (media >=7.0){
+            System.out.println("Você foi Aprovado");
+        }else{
+            System.out.println("Você foi Reprovado");
+        }
+    }
+
+
+
+    }
+
+
+
+
